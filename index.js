6d41f64d3e82b3e4c2aaabc685e7c01ee46c7549
Glasss87 <44882277+Glasss87@users.noreply.github.com>
2019-05-23 20:37:42 -0300

    popular list

--- index.js
+++ index.js
@@ -2,53 +2,21 @@ const apiKey = 'd1baa5f45cdb7ee2c85290a0dfdcbfb8';
 const urlPopular = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
 
 
-
 const input = document.querySelector('#search');
-const list = document.querySelector('movies_list');
-const image = document.querySelector('.movies_list .movies_poster img');
-const title = document.querySelector('.movies_list .movies_content p');
-const movieHeader = document.querySelector('.movies_header');
-
+const movieList = document.querySelector('.movies .movies_list');
+const imageUrl = 'https://image.tmdb.org/t/p/original' ;
 
 fetch(urlPopular)
     .then(res => res.json())
     .then(data => {
-        const movies = data.results;
-        
         const primerasPelis = data.results.slice(0, 5);
-
-        movieHeader.innerHTML = '';
-
-        for (let i = 0; i < movies.length; i++) {
-            const movie = movies[i];
-
-            const ul = document.createElement('ul');
-            const lista = document.createElement('li');
-
-            lista.innerHTML = '<li class="movies_item"><a href=""><div class="movies_poster"><img src="" /></div><div class="movies_content"><p class="movies_title"></p></div></a></li>';
-
-            const ulis = ul.appendChild(lista);
-
-            
+        console.log(primerasPelis);
+        movieList.innerHTML = primerasPelis.map(movie => `<li class="movies_item" id="${movie.id}"><a href=""><div class="movies_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}"/></div><div class="movies_content"><p class="movies_title">${movie.title}</p></div></a></li>`).join('');
 
 
-
-            movieHeader.innerHTML += ulis;
-
-        }
-
-        image.src = 'https://image.tmdb.org/t/p/original' + movies.poster_path;
-        title.innerText = movies.title;
+        
 
 
 
 
     })
-
-
-
-
-//             if (splice) {
-//                 movieObject.splice(4,15)
-//             }
-
