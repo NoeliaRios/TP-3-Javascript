8312f5be55529ac813ca5d57c95edd2a57ecc5f8
Glasss87 <44882277+Glasss87@users.noreply.github.com>
2019-05-24 17:16:50 -0300

    fixed position

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const apiKey = 'd1baa5f45cdb7ee2c85290a0dfdcbfb8';
 const urlPopular = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+const urlTopRated = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`;
+const urlUpcoming = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}`;
+const urlNowPlaying = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`;
+
 
 const input = document.querySelector('#search');
 const movieList = document.querySelector('.movies .movies_list');
+const listTopRated = document.querySelector('.movies .movies_list_toprated');
+const listUpcoming = document.querySelector('.movies .movies_list_upcoming');
+const listNowPlaying = document.querySelector('.movies .movies_list_nowplaying');
 
 
 fetch(urlPopular)
@@ -11,6 +18,33 @@ fetch(urlPopular)
         const primerasPelis = data.results.slice(0, 5);
         console.log(primerasPelis);
         movieList.innerHTML = primerasPelis.map(movie => `<li class="movies_item" id="${movie.id}"><a href=""><div class="movies_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}"/></div><div class="movies_content"><p class="movies_title">${movie.title}</p></div></a></li>`).join('');
+
+    })
+
+fetch(urlTopRated)
+    .then(res => res.json())
+    .then(data => {
+        const primerasPelis = data.results.slice(0, 5);
+        console.log(primerasPelis);
+        listTopRated.innerHTML = primerasPelis.map(movie => `<li class="movies_item" id="${movie.id}"><a href=""><div class="movies_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}"/></div><div class="movies_content"><p class="movies_title">${movie.title}</p></div></a></li>`).join('');
+
+    })
+
+fetch(urlUpcoming)
+    .then(res => res.json())
+    .then(data => {
+        const primerasPelis = data.results.slice(0, 5);
+        console.log(primerasPelis);
+        listUpcoming.innerHTML = primerasPelis.map(movie => `<li class="movies_item" id="${movie.id}"><a href=""><div class="movies_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}"/></div><div class="movies_content"><p class="movies_title">${movie.title}</p></div></a></li>`).join('');
+
+    })
+
+fetch(urlNowPlaying)
+    .then(res => res.json())
+    .then(data => {
+        const primerasPelis = data.results.slice(0, 5);
+        console.log(primerasPelis);
+        listNowPlaying.innerHTML = primerasPelis.map(movie => `<li class="movies_item" id="${movie.id}"><a href=""><div class="movies_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}"/></div><div class="movies_content"><p class="movies_title">${movie.title}</p></div></a></li>`).join('');
 
     })
 
