6f2c5562081007e5fb8f629e60f61efb4424a0a1
Glasss87 <44882277+Glasss87@users.noreply.github.com>
2019-05-23 22:16:14 -0300

    popular

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const apiKey = 'd1baa5f45cdb7ee2c85290a0dfdcbfb8';
 const urlPopular = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
 
-
 const input = document.querySelector('#search');
 const movieList = document.querySelector('.movies .movies_list');
-const imageUrl = 'https://image.tmdb.org/t/p/original' ;
+
 
 fetch(urlPopular)
     .then(res => res.json())
@@ -13,10 +12,7 @@ fetch(urlPopular)
         console.log(primerasPelis);
         movieList.innerHTML = primerasPelis.map(movie => `<li class="movies_item" id="${movie.id}"><a href=""><div class="movies_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}"/></div><div class="movies_content"><p class="movies_title">${movie.title}</p></div></a></li>`).join('');
 
-
-        
-
-
-
-
     })
+
+
+
