f846487b14e7c29151b0bd119e0aafb65ae17883
Glasss87 <44882277+Glasss87@users.noreply.github.com>
2019-05-26 18:18:27 -0300

    view

--- index.js
+++ index.js
@@ -9,56 +9,72 @@ const divResults = document.querySelector('#search_page');
 const ulSearch = document.querySelector('.results_list');
 const photoHome = document.querySelector('.container #photo-home');
 const movieDivs = document.querySelectorAll('.container #movies');
+const homeLogo = document.querySelector('.logo');
+
+
 const movieList = document.querySelector('#movies .movies_list');
 const listTopRated = document.querySelector('#movies .movies_list_toprated');
 const listUpcoming = document.querySelector('#movies .movies_list_upcoming');
 const listNowPlaying = document.querySelector('#movies .movies_list_nowplaying');
 
-
-input.addEventListener('keypress', function(e){
-    if (e.keyCode === 13) {  
-     const q = input.value;
-
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=a70dbfe19b800809dfdd3e89e8532c9e&query=${q}`)
-        .then(res=> res.json())
-        .then(data=> {
-            const movies = data.results;
-            
-            ulSearch.innerHTML = movies.map(movie => `<li class="movies_item" id="${movie.id}"><a href=""><div class="movies_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}"/></div><div class="movies_content"><p class="movies_title">${movie.title}</p></div></a></li>`).join('');
-            divResults.classList.remove('displayNone');
-            
-            divResults.classList.remove('displayNone');
-            
-            for (var i = 0; i < movieDivs.length; ++i) {
-                movieDivs[i].classList.add('displayNone');
-             }
-            photoHome.classList.add('displayNone');
+const popularPage = document.querySelector('#popular_page');
+const popularResults = document.querySelector('#popular_results');
+const viewPopular = document.querySelector('#view_popular');
 
 
 
 
-            
-            
-           
-        })
-    }    
+input.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13) {
+        const q = input.value;
+
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=a70dbfe19b800809dfdd3e89e8532c9e&query=${q}`)
+            .then(res => res.json())
+            .then(data => {
+                const movies = data.results;
+
+                ulSearch.innerHTML = movies.map(movie => `<li class="movies_item" id="${movie.id}"><a href=""><div class="movies_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}"/></div><div class="movies_content"><p class="movies_title">${movie.title}</p></div></a></li>`).join('');
+                divResults.classList.remove('displayNone');
+
+                divResults.classList.remove('displayNone');
+
+                for (var i = 0; i < movieDivs.length; ++i) {
+                    movieDivs[i].classList.add('displayNone');
+                }
+                photoHome.classList.add('displayNone');
+            })
+    }
 })
+
+homeLogo.onclick = function (){
+    divResults.classList.add('displayNone');
+
+}
+
+
 
 
 fetch(urlPopular)
     .then(res => res.json())
     .then(data => {
         const primerasPelis = data.results.slice(0, 5);
-        
+
         movieList.innerHTML = primerasPelis.map(movie => `<li class="movies_item" id="${movie.id}"><a href=""><div class="movies_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}"/></div><div class="movies_content"><p class="movies_title">${movie.title}</p></div></a></li>`).join('');
 
+        const movies = data.results;
+        popularResults.innerHTML = movies.map(movie =>`<li class="movies_item" id="${movie.id}"><a href=""><div class="movies_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}"/></div><div class="movies_content"><p class="movies_title">${movie.title}</p></div></a></li>`).join('');
+        viewPopular.onclick = function(){
+            popularPage.classList.remove('displayNone');
+            photoHome.classList.add('displayNone');
+            
+        }
     })
 
 fetch(urlTopRated)
     .then(res => res.json())
     .then(data => {
         const primerasPelis = data.results.slice(0, 5);
-        
+
         listTopRated.innerHTML = primerasPelis.map(movie => `<li class="movies_item" id="${movie.id}"><a href=""><div class="movies_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}"/></div><div class="movies_content"><p class="movies_title">${movie.title}</p></div></a></li>`).join('');
 
     })
@@ -67,7 +83,7 @@ fetch(urlUpcoming)
     .then(res => res.json())
     .then(data => {
         const primerasPelis = data.results.slice(0, 5);
-        
+
         listUpcoming.innerHTML = primerasPelis.map(movie => `<li class="movies_item" id="${movie.id}"><a href=""><div class="movies_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}"/></div><div class="movies_content"><p class="movies_title">${movie.title}</p></div></a></li>`).join('');
 
     })
@@ -76,7 +92,7 @@ fetch(urlNowPlaying)
     .then(res => res.json())
     .then(data => {
         const primerasPelis = data.results.slice(0, 5);
-        
+
         listNowPlaying.innerHTML = primerasPelis.map(movie => `<li class="movies_item" id="${movie.id}"><a href=""><div class="movies_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}"/></div><div class="movies_content"><p class="movies_title">${movie.title}</p></div></a></li>`).join('');
 
     })
