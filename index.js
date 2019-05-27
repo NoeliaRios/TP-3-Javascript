28560635220a4ac32370469d7735e740393da194
Glasss87 <44882277+Glasss87@users.noreply.github.com>
2019-05-27 19:40:57 -0300

    funtion

--- index.js
+++ index.js
@@ -192,6 +192,7 @@ fetch(urlUpcoming)
 
         listUpcoming.innerHTML = primerasPelis.map(movie => `<li class="movies_item" id="${movie.id}"><a href=""><div class="movies_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}"/></div><div class="movies_content"><p class="movies_title">${movie.title}</p></div></a></li>`).join('');
 
+        
     })
 
 fetch(urlNowPlaying)
